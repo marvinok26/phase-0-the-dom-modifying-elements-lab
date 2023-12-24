fe9cf5efd757280a363bc201c8fdb3e79123ecfb
Marvin <okongomarvin971@gmail.com>
2023-12-24 10:11:35 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const rmMain = document.getElementById('main');
+rmMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id='victory';
+newHeader.textContent='YOUR-NAME is the champion';
+
+document.body.append(newHeader);
